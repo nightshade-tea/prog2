a41f6b4e945b53708ec709bdb304e281b4c9a7f9
pedro f <phfr24@inf.ufpr.br>
2025-03-24 11:19:15 -0300

    update deque.h

--- exs/deque/deque.h
+++ exs/deque/deque.h
@@ -13,20 +13,22 @@ typedef struct _deque {
     nodo *fim;
 } deque;
 
+// aloca um deque vazio
 deque *dq_cria(void);
 
+// libera a memoria do deque e seus nodos, aterra o ponteiro
 void dq_destroi(deque **pdq);
 
-// insere/retira retornam a tamanho do deque apos a operacao
-
+// insere/retira retornam a tamanho do deque apos a operacao ou -1 em erro
 int dq_insere_inicio(deque *dq, int val);
 
-int dq_retira_inicio(deque *dq, int val);
+int dq_insere_fim(deque *dq, int val);
 
-int dq_insere_fim(deque *dq, int *val);
+int dq_retira_inicio(deque *dq, int *val);
 
 int dq_retira_fim(deque *dq, int *val);
 
+// retorna o tamanho do deque ou -1 em erro
 int dq_tamanho(deque *dq);
 
 #endif // !DEQUE_H
