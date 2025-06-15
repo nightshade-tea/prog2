0a266e8aa98d85cba732246446d44bcbb24211cf
phfr24 <phfr24@inf.ufpr.br>
2025-06-15 18:28:57 -0300

    rectangle bouncer

--- A3/pato.c
+++ A3/pato.c
@@ -3,30 +3,26 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 
+#include <time.h>
+
 #include "lib/common.h"
 
 #define FPS 30.0
 #define RES_X 640
 #define RES_Y 480
 
-typedef enum BOUNCER_TYPE
+/* (x1, y1) ----------+
+ * |                  |
+ * |                  |
+ * +-----------(x2, y2) */
+typedef struct OBJECT
 {
-  BT_HELLO = 0,
-  BT_MYSHA,
-  BT_TRIANGLE,
-  BT_RECTANGLE,
-  BT_CIRCLE,
-  BT_LINE1,
-  BT_LINE2,
-  BT_SZ
-} BOUNCER_TYPE;
-
-typedef struct BOUNCER
-{
-  float x, y;
-  float dx, dy;
-  BOUNCER_TYPE type;
-} BOUNCER;
+  int x1, y1;   // upper left point
+  int x2, y2;   // lower right point
+  int xsz, ysz; // x2 - x1, y2 - y1
+  int vx, vy;   // movement speed vector
+  ALLEGRO_COLOR color;
+} OBJECT;
 
 int
 main ()
@@ -34,31 +30,30 @@ main ()
   ALLEGRO_TIMER *timer;
   ALLEGRO_EVENT_QUEUE *queue;
   ALLEGRO_DISPLAY *disp;
-  ALLEGRO_FONT *font;
   ALLEGRO_EVENT event;
-  ALLEGRO_BITMAP *mysha;
-  BOUNCER bvec[BT_SZ];
-  BOUNCER *b;
+
+  ALLEGRO_FONT *font;
+  OBJECT obj;
 
   bool redraw = true;
   bool done = false;
 
+  srand (time (NULL));
   ensure (al_init ());
   ensure (al_install_keyboard ());
   ensure (al_init_image_addon ());
 
   ensure (font = al_create_builtin_font ());
-  ensure (mysha = al_load_bitmap ("assets/mysha.png"));
 
-  for (BOUNCER_TYPE i = 0; i < BT_SZ; i++)
-    {
-      b = &bvec[i];
-      b->x = rand () % RES_X;
-      b->y = rand () % RES_Y;
-      b->dx = ((((float)rand ()) / RAND_MAX) - 0.5) * 8;
-      b->dy = ((((float)rand ()) / RAND_MAX) - 0.5) * 8;
-      b->type = i;
-    }
+  obj.xsz = (RES_X / 10) + rand () % (RES_X / 10);
+  obj.ysz = (RES_Y / 10) + rand () % (RES_Y / 10);
+  obj.x1 = rand () % (RES_X - obj.xsz);
+  obj.y1 = rand () % (RES_Y - obj.ysz);
+  obj.x2 = obj.x1 + obj.xsz;
+  obj.y2 = obj.y1 + obj.ysz;
+  obj.vx = rand () % (RES_X / 70);
+  obj.vy = rand () % (RES_Y / 70);
+  obj.color = al_map_rgb (0, 0, 255);
 
   ensure (timer = al_create_timer (1.0 / FPS));
   ensure (queue = al_create_event_queue ());
@@ -77,35 +72,37 @@ main ()
       switch (event.type)
         {
         case ALLEGRO_EVENT_TIMER:
-          for (BOUNCER_TYPE i = 0; i < BT_SZ; i++)
+          obj.x1 += obj.vx;
+          obj.y1 += obj.vy;
+          obj.x2 += obj.vx;
+          obj.y2 += obj.vy;
+
+          if (obj.x1 < 0)
             {
-              b = &bvec[i];
-              b->x += b->dx;
-              b->y += b->dy;
+              obj.x1 *= -1;
+              obj.x2 = obj.x1 + obj.xsz;
+              obj.vx *= -1;
+            }
 
-              if (b->x < 0)
-                {
-                  b->x *= -1;
-                  b->dx *= -1;
-                }
+          else if (obj.x2 > RES_X)
+            {
+              obj.x2 = 2 * RES_X - obj.x2;
+              obj.x1 = obj.x2 - obj.xsz;
+              obj.vx *= -1;
+            }
 
-              if (b->x > RES_X)
-                {
-                  b->x = 2 * RES_X - b->x;
-                  b->dx *= -1;
-                }
+          if (obj.y1 < 0)
+            {
+              obj.y1 *= -1;
+              obj.y2 = obj.y1 + obj.ysz;
+              obj.vy *= -1;
+            }
 
-              if (b->y < 0)
-                {
-                  b->y *= -1;
-                  b->dy *= -1;
-                }
-
-              if (b->y > RES_Y)
-                {
-                  b->y = 2 * RES_Y - b->y;
-                  b->dy *= -1;
-                }
+          else if (obj.y2 > RES_Y)
+            {
+              obj.y2 = 2 * RES_Y - obj.y2;
+              obj.y1 = obj.y2 - obj.ysz;
+              obj.vy *= -1;
             }
 
           redraw = true;
@@ -121,53 +118,15 @@ main ()
         {
           al_clear_to_color (al_map_rgb (0, 0, 0));
 
-          for (BOUNCER_TYPE i = 0; i < BT_SZ; i++)
-            {
-              b = &bvec[i];
-              switch (b->type)
-                {
-                case BT_HELLO:
-                  al_draw_text (font, al_map_rgb (255, 255, 255), b->x, b->y,
-                                ALLEGRO_ALIGN_LEFT, "pato sapato v0.1");
-                  break;
-
-                case BT_MYSHA:
-                  al_draw_bitmap (mysha, b->x, b->y, 0);
-                  break;
-
-                case BT_TRIANGLE:
-                  al_draw_filled_triangle (b->x, b->y, b->x + 50, b->y + 25,
-                                           b->x, b->y + 50,
-                                           al_map_rgb_f (0, 1, 0));
-                  break;
-
-                case BT_RECTANGLE:
-                  al_draw_filled_rectangle (b->x, b->y, b->x + 100, b->y + 80,
-                                            al_map_rgba_f (0, 0, 0.5, 0.5));
-                  break;
-
-                case BT_CIRCLE:
-                  al_draw_circle (b->x, b->y, 30, al_map_rgb_f (1, 0, 1), 2);
-                  break;
-
-                case BT_LINE1:
-                  al_draw_line (b->x, b->y, b->x + 20, b->y + 100,
-                                al_map_rgb_f (1, 0, 0), 1);
-                  break;
-
-                case BT_LINE2:
-                  al_draw_line (b->x, b->y, b->x + 70, b->y - 20,
-                                al_map_rgb_f (1, 1, 0), 1);
-                  break;
-                }
-            }
+          al_draw_text (font, al_map_rgb (255, 255, 255), 10, 10,
+                        ALLEGRO_ALIGN_LEFT, "pato sapato v0.1");
+          al_draw_filled_rectangle (obj.x1, obj.y1, obj.x2, obj.y2, obj.color);
 
           al_flip_display ();
           redraw = false;
         }
     }
 
-  al_destroy_bitmap (mysha);
   al_destroy_font (font);
   al_destroy_display (disp);
   al_destroy_timer (timer);
